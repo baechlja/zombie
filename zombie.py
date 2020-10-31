d2312b5263d5a2b8961a1b6bdeb201d0f4bc248e
baechlja <73224461+baechlja@users.noreply.github.com>
2020-10-31 11:52:02 +0100

    Add files via upload

---
+++ zombie.py
@@ -0,0 +1,44 @@
+import  re
+from random import choice
+import requests
+
+url = "gutenberg.txt"
+r = open(url,encoding= "utf8").read()
+text = r
+
+
+plural_nouns = ["ladies", "gentlemen", "women", "men", "children", "boys", "girls"]
+
+singular_nouns = ["son", "daughter", "child", "wife", "woman", "mrs", "miss", "husband", "man", "mr", "sir", "lady"]
+
+speaking = ["said", "replied", "spoke", "shouted", "cried", "whispered"]
+
+zombie_sounds = ["groaned", "moaned", "growled", "screamed", "gurgled"]
+
+plural_nouns = plural_nouns + [word.title() for word in plural_nouns]
+
+singular_nouns = singular_nouns + [word.title() for word in singular_nouns]
+
+for word in plural_nouns:
+    text = re.sub(r'\b{0}\b'.format(word), "zombies", text)
+    
+for word in singular_nouns:
+    text= re.sub(r'\b{0}\b'.format(word), "zombie", text)
+
+for word in speaking:
+    text = re.sub(r'\b{0}\b'.format(word), choice(zombie_sounds), text)
+
+def zombify_speech(text):
+    text = re.sub(r'[eiosEIOS]', "r", text)
+    text = re.sub(r'[^zhrgbmnaZHRGBMNA“”?\n.!?-]', '', text)
+    text = re.sub(r'r\b', 'rh', text)
+    text = re.sub(r'(\b[aA]\b)', 'hra', text)
+    return(text)
+
+speech = re.findall(r'“.*?”', text, flags=re.DOTALL)
+
+for words in speech:
+    text = text.replace(words,zombify_speech(words))
+
+with open("Zombie.txt", "w", encoding="utf-8") as f:
+    f.write(text)
