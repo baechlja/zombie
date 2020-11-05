811a62460b1357678b6c282fde55e2d4408b0e99
Jasmin Bächle <jasmin-baechle@web.de>
2020-11-05 20:47:46 +0100

    update with comments

--- zombie.py
+++ zombie.py
@@ -2,32 +2,39 @@ import  re
 from random import choice
 import requests
 
+#import the file
 url = "gutenberg.txt"
 r = open(url,encoding= "utf8").read()
 text = r
 
 
-plural_nouns = ["ladies", "gentlemen", "women", "men", "children", "boys", "girls"]
 
-singular_nouns = ["son", "daughter", "child", "wife", "woman", "mrs", "miss", "husband", "man", "mr", "sir", "lady"]
+#create lists and replace normal words with zombiewords
+def change_prose(text):
+    plural_nouns = ["ladies", "gentlemen", "women", "men", "children", "boys", "girls"]
 
-speaking = ["said", "replied", "spoke", "shouted", "cried", "whispered"]
+    singular_nouns = ["son", "daughter", "child", "wife", "woman", "mrs", "miss", "husband", "man", "mr", "sir", "lady"]
 
-zombie_sounds = ["groaned", "moaned", "growled", "screamed", "gurgled"]
+    speaking = ["said", "replied", "spoke", "shouted", "cried", "whispered"]
 
-plural_nouns = plural_nouns + [word.title() for word in plural_nouns]
+    zombie_sounds = ["groaned", "moaned", "growled", "screamed", "gurgled"]
 
-singular_nouns = singular_nouns + [word.title() for word in singular_nouns]
+    plural_nouns = plural_nouns + [word.title() for word in plural_nouns]
 
-for word in plural_nouns:
-    text = re.sub(r'\b{0}\b'.format(word), "zombies", text)
+    singular_nouns = singular_nouns + [word.title() for word in singular_nouns]
+
+    for word in plural_nouns:
+        text = re.sub(r'\b{0}\b'.format(word), "zombies", text)
     
-for word in singular_nouns:
-    text= re.sub(r'\b{0}\b'.format(word), "zombie", text)
+    for word in singular_nouns:
+        text= re.sub(r'\b{0}\b'.format(word), "zombie", text)
 
-for word in speaking:
-    text = re.sub(r'\b{0}\b'.format(word), choice(zombie_sounds), text)
+    for word in speaking:
+        text = re.sub(r'\b{0}\b'.format(word), choice(zombie_sounds), text)
+    return(text)
 
+
+#replace diffrent letters
 def zombify_speech(text):
     text = re.sub(r'[eiosEIOS]', "r", text)
     text = re.sub(r'[^zhrgbmnaZHRGBMNA“”?\n.!?-]', '', text)
@@ -40,5 +47,8 @@ speech = re.findall(r'“.*?”', text, flags=re.DOTALL)
 for words in speech:
     text = text.replace(words,zombify_speech(words))
 
+text = change_prose(text)
+
+#create a new file calles "Zombie"
 with open("Zombie.txt", "w", encoding="utf-8") as f:
     f.write(text)
